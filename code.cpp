abe0bfcca23e3d7b2e9e8cb6133e0a117baacf15
Maleek Hussain <i221526@nu.edu.pk>
2023-09-01 21:22:41 +0500

    implemented binary search

--- code.cpp
+++ code.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 using namespace std;
 
+void selection_sort(int arr[], int size) {
+    // int size = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < size; i++) {
+        for (int j = i; j < size; j++) {
+            if (arr[i] > arr[j]) {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
 
-
-int binary_search(int arr[], int key) {
-    int size = sizeof(arr) / sizeof(arr[0]);
+int binary_search(int arr[], int key, int size) {
     int start = 0, end = size - 1, mid = (start + end) / 2;
 
     while(1) {
         if (arr[mid] == key) {
             return mid;
         } 
+        else if(start == end)
+        {
+            return -1;
+        }
         else if (arr[mid] > key) {
             end = mid - 1;
         }
         else if (arr[mid] < key) {
             start = mid + 1;
         }
-        else if (start > end) {
-            return -1;
-        }
+        mid = (start + end) / 2;
     }
 }
 
 int main() {
     int arr[] = {2, 4, 5, -6, 0, -2, 8, 3, 9, 1, -3};
-    int key = 4;
-    cout << "key " << key << " found at index location " << binary_search(arr, key);
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 99;
+    selection_sort(arr, size);
+    for (int i = 0; i < size; i++) {
+        cout << endl << "At index " << i << ": " << arr[i];
+    }
+    cout << endl << "key " << key << " found at index location " << binary_search(arr, key, size);
 }
