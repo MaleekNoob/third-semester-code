153319b5d4010ee35cde1e51335140d3e62b2c81
Maleek Hussain <i221526@nu.edu.pk>
2023-09-07 22:14:41 +0500

    reading file functionality

--- Assignment-1/case-scenario-1.cpp
+++ Assignment-1/case-scenario-1.cpp
@@ -15,9 +15,18 @@ int main() {
     }
 
     while(!file.eof()) {
-        string name;
-        int age;
-        file >> name >> age;
-        cout << name << " " << age << endl;
+        string name, id, salary, date, job_title, temp;
+        getline(file, name, '\n');
+        getline(file, id, '\n');
+        getline(file, salary, '\n');
+        getline(file, date, '\n');
+        getline(file, job_title, '\n');
+        getline(file, temp, '\n');
+
+        cout << endl << endl << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+        cout << "Salary: " << salary << endl;
+        cout << "Date: " << date << endl;
+        cout << "Job Title: " << job_title << endl;
     }
 }
