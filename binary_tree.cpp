5197b8117c24f3e1103bbfeb307309936fc28a28
MaleekNoob <maleekhussainali456@gmail.com>
2023-10-16 20:28:45 +0500

    practice for lab sessional exam

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,63 +1,69 @@
 #include <iostream>
 using namespace std;
 
-// Definition of the node
 template <class T>
-struct binaryTreeNode
-{
-    T info;
-    binaryTreeNode<T> *left;
-    binaryTreeNode<T> *right;
-};
-
-// Definition of the class
-template <class T>
-class binaryTreeType
-{
+class Node {
 public:
-    const binaryTreeType<T> &operator=(const binaryTreeType<T> &); // Overload the assignment operator.
+    T data;
+    Node* right;
+    Node* left;
 
-    bool isEmpty() const; // Returns true if the binary tree is empty; otherwise, returns false.
-
-    void inorderTraversal() const; // Function to do an inorder traversal of the binary tree.
-
-    void preorderTraversal() const; // Function to do a preorder traversal of the binary tree.
-
-    void postorderTraversal() const; // Function to do a postorder traversal of the binary tree.
-
-    int treeHeight() const; // Returns the height of the binary tree.
-
-    int treeNodeCount() const; // Returns the number of nodes in the binary tree.
-
-    int treeLeavesCount() const; // Returns the number of leaves in the binary tree.
-
-    void destroyTree(); // Deallocates the memory space occupied by the binary tree; Postcondition: root = NULL;
-
-    binaryTreeType(const binaryTreeType<T> &otherTree); // copy constructor
-
-    binaryTreeType(); // default constructor
-
-    ~binaryTreeType(); // destructor
-
-protected:
-    binaryTreeNode<T> *root;
-
-private:
-    void copyTree(binaryTreeNode<T> * &copiedTreeRoot, binaryTreeNode<T> * otherTreeRoot); // Makes a copy of the binary tree to which otherTreeRoot points. The pointer copiedTreeRoot points to the root of the copied binary tree.
-
-    void destroy(binaryTreeNode<T> * &p); // Function to destroy the binary tree to which p points. Postcondition: p = NULL
-    
-    void inorder(binaryTreeNode<T> * p) const; // Function to do an inorder traversal of the binary tree to which p points.
-
-    void preorder(binaryTreeNode<T> * p) const; // Function to do a preorder traversal of the binary tree to which p points.
-
-    void postorder(binaryTreeNode<T> * p) const; // Function to do a postorder traversal of the binary tree to which p points.
-
-    int height(binaryTreeNode<T> * p) const; // Function to return the height of the binary tree to which p points.
-
-    int max(int x, int y) const; // Returns the larger of x and y.
-
-    int nodeCount(binaryTreeNode<T> * p) const; // Function to return the number of nodes in the binary tree to which p points
-
-    int leavesCount(binaryTreeNode<T> * p) const; // Function to return the number of leaves in the binary tree to which p points
+    Node(T data) {
+        this->data = data;
+        this->right = NULL;
+        this->left = NULL;
+    }
 };
+
+template <class T>
+class BinaryTree {
+    private:
+    Node<T>* root;
+
+    public:
+    BinaryTree() {
+        this->root = NULL;
+    }
+
+    BinaryTree(Node<T>* root) {
+        this->root = root;
+    }
+
+    Node<T>* BuildTree() {
+        T data;
+        cout << "\nEnter data (Enter -1 for NULL): ";
+        cin >> data;
+        root = new Node<T>(data);
+
+        if (data == -1) {
+            return NULL;
+        }
+
+        cout << "\nData for Left of " << data;
+        root->left = BuildTree();
+        cout << "\nData for Right of " << data;
+        root->right = BuildTree();
+        return root;
+    }
+
+    void print() {
+        printTree(root);
+    }
+
+    void printTree(Node<T>* root) {
+        if (root == NULL) {
+            return;
+        }
+
+        cout << root->data << " ";
+        printTree(root->left);
+        printTree(root->right);
+    }
+
+};
+
+int main() {
+    BinaryTree<int> tree;
+    tree.BuildTree();
+    tree.print();
+}
---
+++ lab-sessional-preparation.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Node {
+    public:
+    T data;
+    Node* next;
+
+    Node () {
+        data = NULL;
+        next = NULL;
+    }
+
+    Node (T data) {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+template <class T>
+class Stack {
+    private:
+    Node<T>* top;
+
+    public:
+    Stack() {
+        top = NULL;
+    }
+
+    Stack(Stack* stack) {
+        top = stack;
+    }
+
+    void push(T data) {
+        Node<T>* temp = new Node(data);
+
+        if (isEmpty()) {
+            top = temp;
+        }
+        else {
+            temp->next = top;
+            top = temp;
+        }
+    }
+
+    T pop()
+    {
+        if (isEmpty())
+        {
+            cout << "ERor";
+        }
+        Node<T> *toPop = top;
+        top = top->next;
+        T var = toPop->data;
+        delete toPop;
+        return var;
+    }
+
+    Node<T>* Top() {
+        return top;
+    }
+
+    T peek() {
+        T var = top->data;
+        return var;
+    }
+
+    int size() {
+        Node<T> *traverse = top;
+        int count = 0;
+        while (traverse != NULL) {
+            count++;
+            traverse = traverse->next;
+        }
+        return count;
+    }
+
+    bool isEmpty() {
+        if (top == NULL) {
+            return true;
+        }
+        return false;
+    }
+
+    void print() {
+        Node<T>* traverse = top;
+        while (traverse != NULL) {
+            cout << endl << traverse->data;
+            traverse = traverse->next;
+        }
+    }
+};
+
+template <class T>
+void generic_print(Stack<T>& stack) {
+    if (!stack.isEmpty()) {
+        cout << endl << stack.peek();
+        T temp = stack.pop();
+        generic_print(stack);
+        stack.push(temp);
+    }
+}
+
+template <class T2>
+void insertAtEnd(Stack<T2>& stack, T2 value) {
+    if (stack.isEmpty()) {
+        stack.push(value);
+        return;
+    }
+    T2 temp = stack.pop();
+    insertAtEnd(stack, value);
+    stack.push(temp);
+}
+
+template <class T1>
+void reverse_stack(Stack<T1>& stack) {
+    if (!stack.isEmpty()) {
+        T1 temp = stack.pop();
+        reverse_stack(stack);
+        insertAtEnd(stack, temp);
+    }
+}
+
+int main() {
+
+    Stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    generic_print(s);
+    reverse_stack(s);
+    cout << "Stack reversed!";
+    generic_print(s);
+    return 0;
+}
