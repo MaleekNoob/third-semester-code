5f36fb7c5f0c3924e67ee030f5ad8d15ba0bb10c
MaleekNoob <maleekhussainali456@gmail.com>
2023-10-19 14:00:38 +0500

    sorted linked list using insertion sort

--- lab-sessional-reverse-linked-list.cpp
+++ lab-sessional-reverse-linked-list.cpp
@@ -6,6 +6,11 @@ public:
     int data;
     Node* next;
 
+    Node() {
+        this->data = -1;
+        next = NULL;
+    }
+
     Node(int data) {
         this->data = data;
         next = NULL;
@@ -16,8 +21,10 @@ class List {
     private:
     public:
     Node* head;
+    Node* sorted;
 
     List() {
+        sorted = NULL;
         head = NULL;
     }
 
@@ -97,6 +104,48 @@ class List {
 
         return false;
     }
+
+    void InputInSortedList(Node *newNode)
+    {
+        Node *newNode1 = new Node();
+        newNode1->data = newNode->data;
+        newNode1->next = NULL;
+        if (sorted == NULL || sorted->data >= newNode1->data)
+        {
+            newNode1->next = sorted;
+            sorted = newNode1;
+        }
+        else
+        {
+            Node *temp = sorted;
+            while (temp->next != NULL && temp->next->data < newNode1->data)
+            {
+                temp = temp->next;
+            }
+            newNode1->next = temp->next;
+            temp->next = newNode1;
+        }
+    }
+    
+    Node *insertionSortList(Node *head)
+    {
+        Node *traverse = head;
+        sorted = NULL;
+        while (traverse != NULL)
+        {
+            InputInSortedList(traverse);
+            cout << "\nSorted list be ";
+            Node* sortTraverse = sorted;
+            while (sortTraverse != NULL ) {
+                cout << sortTraverse->data << " ";
+                sortTraverse = sortTraverse->next;
+            }
+            traverse = traverse->next;
+        }
+        head = sorted;
+
+        return head;
+    }
 };
 
 Node *reverseList(Node *head)
@@ -114,14 +163,20 @@ Node *reverseList(Node *head)
 int main() {
 
     List list;
-    list.insertAtEnd(1);
-    list.insertAtEnd(2);
+    list.insertAtEnd(-1);
+    list.insertAtEnd(5);
     list.insertAtEnd(3);
     list.insertAtEnd(4);
-    list.insertAtEnd(5);
+    list.insertAtEnd(0);
     list.print();
-    reverseList(list.head);
-    list.print();
+    Node* kamal = list.insertionSortList(list.head);
+    
+    Node* traverse = kamal;
+    cout << endl << "Sorted List: ";
+    while (traverse != NULL) {
+        cout << traverse->data << " ";
+        traverse = traverse->next;
+    }
 
     return 0;
 }
