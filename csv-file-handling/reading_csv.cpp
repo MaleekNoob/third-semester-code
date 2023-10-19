6c13c4f52828bd88d954e07c3716d7999f238d68
MaleekNoob <maleekhussainali456@gmail.com>
2023-10-19 22:51:03 +0500

    error in reading file program

--- csv-file-handling/reading_csv.cpp
+++ csv-file-handling/reading_csv.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+// #include <cstring>
 using namespace std;
 
 void displayFileData(fstream& file) {
@@ -16,6 +17,144 @@ void displayFileData(fstream& file) {
     }
 }
 
+void StoreDataInArray(fstream& file) {
+    string arr[300];
+    int i = 0;
+    while (!file.eof()) {
+        string line = "";
+        getline(file, line);
+        for (int i = 0; i < line.length(); i++) {
+            if (line[i] == ',') {
+                line[i] = ' ';
+            }
+        }
+        arr[i] = line;
+        i++;
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << arr[i];
+    }
+
+    // Store data of each column in a separate array
+    // #,claim_status,video_id,video_duration_sec,video_transcription_text,verified_status,author_ban_status,video_view_count,video_like_count,video_share_count,video_download_count,video_comment_count
+    string claim_status[300];
+    string video_id[300];
+    string video_duration_sec[300];
+    string video_transcription_text[300];
+    string verified_status[300];
+    string author_ban_status[300];
+    string video_view_count[300];
+    string video_like_count[300];
+    string video_share_count[300];
+    string video_download_count[300];
+    string video_comment_count[300];
+
+
+    for (int i = 0; i < 300; i++) {
+        int j = 0;
+        while (arr[i][j] != ' ') {
+            claim_status[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        while (arr[i][j] != ' ') {
+            video_id[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        while (arr[i][j] != ' ') {
+            video_duration_sec[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        while (arr[i][j] != ' ') {
+            video_transcription_text[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        while (arr[i][j] != ' ') {
+            verified_status[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        if (j < strlen(arr[i]))
+        { // check if j is within bounds
+            while (arr[i][j] != ' ')
+            {
+                author_ban_status[i] += arr[i][j];
+                j++;
+                if (j >= strlen(arr[i]))
+                { // check if j is within bounds
+                    break;
+                }
+            }
+        }
+        j++;
+        while (arr[i][j] != ' ')
+        {
+            video_view_count[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        while (arr[i][j] != ' ')
+        {
+            video_like_count[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        while (arr[i][j] != ' ')
+        {
+            video_share_count[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        while (arr[i][j] != ' ') {
+            video_download_count[i] += arr[i][j];
+            j++;
+        }
+        j++;
+        while (arr[i][j] != ' ') {
+            video_comment_count[i] += arr[i][j];
+            j++;
+        }
+    }
+
+    // Display data of each column
+    for (int i = 0; i < 300; i++) {
+        cout << endl << claim_status[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << video_id[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << video_duration_sec[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << video_transcription_text[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << verified_status[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << author_ban_status[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << video_view_count[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << video_like_count[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << video_share_count[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << video_download_count[i];
+    }
+    for (int i = 0; i < 300; i++) {
+        cout << endl << video_comment_count[i];
+    }
+}
+
 int main() {
 
     fstream file;
@@ -25,6 +164,7 @@ int main() {
         exit(1);
     }
     displayFileData(file);
+    StoreDataInArray(file);
     file.close();
 
     return 0;
