67d2baeafab41f59652cf1c8fc81cbd5aee9bb93
MaleekNoob <i221526@nu.edu.pk>
2023-10-30 23:00:13 +0500

    deleting node

--- binary-search-tree.cpp
+++ binary-search-tree.cpp
@@ -3,125 +3,166 @@ using namespace std;
 
 class Node {
     public:
-    int data;
-    Node* left;
-    Node* right;
+        int data;
+        Node* left;
+        Node* right;
 
-    Node() {
-        this->data = 0;
-        this->left = nullptr;
-        this->right = nullptr;
-    }
+        Node() {
+            this->data = 0;
+            this->left = NULL;
+            this->right = NULL; 
+        }
 
-    Node(int data) {
-        this->data = data;
-        this->left = nullptr;
-        this->right = nullptr;
-    }
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
 };
 
-class BinarySearchTree {
-    Node* root;
+class BinaryTree {
+    private:
+        Node* root;
 
     public:
-    BinarySearchTree() {
-        this->root = nullptr;
+    BinaryTree() {
+        root = NULL;
     }
 
-    BinarySearchTree(int data) {
-        this->root = new Node(data);
+    BinaryTree(int data) {
+        root = new Node(data);
+    }
+
+    BinaryTree(Node* root) {
+        this->root = root;
     }
 
     void insert(int data) {
-        this->root = insertNode(this->root, data);
-    }
-
-    Node* insertNode(Node* node, int data) {
-        if (node == nullptr) {
-            Node* newNode = new Node(data);
-            return newNode;
+        Node* newNode = new Node(data);
+        if (root == NULL) {
+            root = newNode;
+            return;
         }
-        if (data < node->data) {
-            node->left = insertNode(node->left, data);
-        } else {
-            node->right = insertNode(node->right, data);
-        }
-        return node;
-    }
 
-    void Delete(int data) {
-        this->root = deleteNode(this->root, data);
-    }
-
-    Node* deleteNode(Node* node, int data) {
-        if (node == nullptr) {
-            return nullptr;
-        }
-        if (data < node->data) {
-            node->left = deleteNode(node->left, data);
-        } else if (data > node->data) {
-            node->right = deleteNode(node->right, data);
-        } else {
-            if (node->left == nullptr && node->right == nullptr) {
-                delete node;
-                return nullptr;
-            } else if (node->left == nullptr) {
-                Node* temp = node->right;
-                delete node;
-                return temp;
-            } else if (node->right == nullptr) {
-                Node* temp = node->left;
-                delete node;
-                return temp;
-            } else {
-                Node* temp = node->right;
-                while (temp->left != nullptr) {
-                    temp = temp->left;
+        Node* temp = root;
+        while (temp != NULL) {
+            if (data < temp->data) {
+                if (temp->left == NULL) {
+                    temp->left = newNode;
+                    return;
                 }
-                node->data = temp->data;
-                node->right = deleteNode(node->right, temp->data);
+                temp = temp->left;
+            } else {
+                if (temp->right == NULL) {
+                    temp->right = newNode;
+                    return;
+                }
+                temp = temp->right;
             }
         }
-        return node;
+    }
+
+    Node* Delete(Node* root, int data) {
+        if (root == NULL) {
+            return root;
+        }
+
+        if (data < root->data) {
+            root->left = Delete(root->left, data);
+            return root;
+        }
+        else if (data > root->data) {
+            root->right = Delete(root->right, data);
+            return root;
+        }
+
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        else {
+            Node* succ = root->right;
+            Node* Parentsucc = root;
+
+            while (succ->left != NULL) {
+                Parentsucc = succ;
+                succ = succ->left;
+            }
+
+            if (Parentsucc != root) {
+                Parentsucc->left = succ->right;
+            }
+            else {
+                
+            }
+
+            delete succ;
+            return Parentsucc;
+        }
+        
     }
 
     void print() {
-        Node* address = root;
-        display(address);
+        int choice;
+        cout << "\n1. Preorder\n2. Inorder\n3. Postorder\nEnter your choice: ";
+        cin >> choice;
+        while (choice < 1 || choice > 3) {
+            cout << "Invalid choice. Enter again: ";
+            cin >> choice;
+        }
+
+        switch (choice)
+        {
+        case 1:
+            Preorder(root);
+            break;
+
+        case 2:
+            Inorder(root);
+            break;
+
+        case 3:
+            Postorder(root);
+            break;
+
+        default:
+            break;
+        }
     }
 
-    void display(Node* node) {
-        if (node == NULL) {
+    void Preorder(Node* root) {
+        if (root == NULL) {
             return;
         }
-        display(node->left);
-        cout << " " << node->data;
-        display(node->right);
+
+        cout << root->data << " ";
+        Preorder(root->left);
+        Preorder(root->right);
+    }
+
+    void Inorder(Node* root) {
+        if (root == NULL) {
+            return;
+        }
+
+        Inorder(root->left);
+        cout << root->data << " ";
+        Inorder(root->right);
+    }
+
+    void Postorder(Node* root) {
+        if (root == NULL) {
+            return;
+        }
+
+        Postorder(root->left);
+        Postorder(root->right);
+        cout << root->data << " ";
     }
 };
-
-int main() {
-
-    BinarySearchTree bst;
-    bst.insert(10);
-    bst.insert(11);
-    bst.insert(-5);
-    bst.insert(0);
-    bst.insert(2);
-    bst.insert(77);
-    bst.insert(41);
-    bst.insert(-14);
-    bst.insert(115);
-    bst.insert(-9);
-
-    bst.print();
-    cout << endl;
-
-    bst.Delete(11);
-    bst.Delete(-14);
-    bst.Delete(0);
-
-    bst.print();
-
-    return 0;
-}
